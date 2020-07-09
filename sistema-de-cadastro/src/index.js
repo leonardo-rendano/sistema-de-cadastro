77adbd3e7d33470db5b8dd7cd2dbafc9a1f3df62
Leonardo-Rendano <62267208+Leonardo-Rendano@users.noreply.github.com>
2020-07-08 21:31:39 -0300

    Instalação do framework Bootswatch

--- sistema-de-cadastro/src/index.js
+++ sistema-de-cadastro/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import App from './App';
 
+import 'bootswatch/dist/cerulean/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
