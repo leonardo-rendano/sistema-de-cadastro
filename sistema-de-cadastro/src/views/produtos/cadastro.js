4b2ee1081a0ed2db2027ee7a96585e94abc97216
Leonardo-Rendano <62267208+Leonardo-Rendano@users.noreply.github.com>
2020-07-08 22:43:23 -0300

    FInalização do formulário

--- sistema-de-cadastro/src/views/produtos/cadastro.js
+++ sistema-de-cadastro/src/views/produtos/cadastro.js
@@ -12,14 +12,14 @@ export default class CadastroProdutos extends React.Component {
           <div className="row">
             <div className="col-md-6">
               <div className="form-group">
-                <label>Nome: *</label>
+                <label> Nome: * </label>
                 <input type="text" className="form-control" />
               </div>
             </div>
 
             <div className="col-md-6">
-            <div className="form-group">
-                <label>SKU: *</label>
+              <div className="form-group">
+                <label> SKU: * </label>
                 <input type="text" className="form-control" />
               </div>
             </div>
@@ -28,9 +28,35 @@ export default class CadastroProdutos extends React.Component {
           <div className="row">
             <div className="col-md-12">
               <div className="form-group">
-                <label>Descrição</label>
+                <label> Descrição </label>
                 <textarea className="form-control" />
               </div>
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="col-md-6">
+              <div className="form-group">
+                <label> Preço: * </label>
+                <input type="text" className="form-control" />
+              </div>
+            </div>
+
+            <div className="col-md-6">
+              <div className="form-group">
+                <label> Fornecedor: * </label>
+                <input type="text" className="form-control" />
+              </div>
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="col-md-1">
+              <button className="btn btn-success"> Salvar </button>
+            </div>
+
+            <div className="col-md-1">
+              <button className="btn btn-primary"> Limpar </button>
             </div>
           </div>
 
