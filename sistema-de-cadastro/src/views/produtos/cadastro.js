881167839e649cc81fa1957fc214abde0c27c8ba
Leonardo-Rendano <62267208+Leonardo-Rendano@users.noreply.github.com>
2020-07-09 02:26:26 -0300

    Manipulação dos estados e criação das funções

--- sistema-de-cadastro/src/views/produtos/cadastro.js
+++ sistema-de-cadastro/src/views/produtos/cadastro.js
@@ -1,6 +1,25 @@
 import React from 'react';
 
 export default class CadastroProdutos extends React.Component {
+
+  state = {
+    nome: '',
+    sku: '',
+    descrição: '',
+    preço: 0,
+    fornecedor: ''
+  }
+
+  onChange = (event) => {
+    const valor = event.target.value
+    const nomeDoCampo = event.target.name
+    this.setState({ [nomeDoCampo]: valor })
+  }
+
+  onSubmit = (event) => {
+    console.log(this.state)
+  }
+
   render() {
     return(
       <div className="card">
@@ -13,14 +32,22 @@ export default class CadastroProdutos extends React.Component {
             <div className="col-md-6">
               <div className="form-group">
                 <label> Nome: * </label>
-                <input type="text" className="form-control" />
+                <input type="text"
+                       name="nome" 
+                       onChange={this.onChange}
+                       value={this.state.nome} 
+                       className="form-control" />
               </div>
             </div>
 
             <div className="col-md-6">
               <div className="form-group">
                 <label> SKU: * </label>
-                <input type="text" className="form-control" />
+                <input type="text" 
+                       name="sku" 
+                       onChange={this.onChange}
+                       value={this.state.sku} 
+                       className="form-control" />
               </div>
             </div>
           </div>
@@ -29,7 +56,10 @@ export default class CadastroProdutos extends React.Component {
             <div className="col-md-12">
               <div className="form-group">
                 <label> Descrição </label>
-                <textarea className="form-control" />
+                <textarea name="descrição" 
+                          onChange={this.onChange}
+                          value={this.state.descrição} 
+                          className="form-control" />
               </div>
             </div>
           </div>
@@ -38,21 +68,29 @@ export default class CadastroProdutos extends React.Component {
             <div className="col-md-6">
               <div className="form-group">
                 <label> Preço: * </label>
-                <input type="text" className="form-control" />
+                <input type="text" 
+                       name="preço" 
+                       onChange={this.onChange}
+                       value={this.state.preço} 
+                       className="form-control" />
               </div>
             </div>
 
             <div className="col-md-6">
               <div className="form-group">
                 <label> Fornecedor: * </label>
-                <input type="text" className="form-control" />
+                <input type="text" 
+                       name="fornecedor" 
+                       onChange={this.onChange}
+                       value={this.state.fornecedor} 
+                       className="form-control" />
               </div>
             </div>
           </div>
 
           <div className="row">
             <div className="col-md-1">
-              <button className="btn btn-success"> Salvar </button>
+              <button onClick={this.onSubmit} className="btn btn-success"> Salvar </button>
             </div>
 
             <div className="col-md-1">
