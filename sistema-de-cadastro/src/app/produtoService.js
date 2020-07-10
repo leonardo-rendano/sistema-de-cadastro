e698c2b39e527d182d6207fad75e976d760cb87f
Leonardo-Rendano <62267208+Leonardo-Rendano@users.noreply.github.com>
2020-07-09 21:02:13 -0300

    Implementação da validação do formulário

--- sistema-de-cadastro/src/app/produtoService.js
+++ sistema-de-cadastro/src/app/produtoService.js
@@ -1,9 +1,44 @@
 
 const PRODUTOS = '_PRODUTOS'; // "_PRODUTOS" é a key que será buscada pelo localstorage para salvar os itens posteriormente com o método "setItem"
 
+function ErroValidação(errors) {
+  this.errors = errors;
+}
+
 export default class ProdutoService {
 
+
+  validar = (produto) => {
+    const errors = []
+
+    if(!produto.nome) {
+      errors.push('O campo Nome é obrigatório!')
+    }
+
+    if(!produto.sku) {
+      errors.push('O campo SKU é obrigatório!')
+    }
+
+    if(!produto.descrição) {
+      errors.push('O campo Descrição é obrigatório!')
+    }
+
+    if(!produto.preço || produto.preço <= 0) {
+      errors.push('O campo Preço deve ser maior que zero(0)!')
+    }
+
+    if(!produto.fornecedor) {
+      errors.push('O campo Fornecedor é obrigatório!')
+    }
+
+    if(errors.length > 0) {
+      throw new ErroValidação(errors)
+    }
+  }
+
+
   salvar = (produto) => {
+    this.validar(produto)
 
     let produtos = localStorage.getItem(PRODUTOS) // Foi usada a variável LET, pois o valor será modificado de STRING para ARRAY!
 
