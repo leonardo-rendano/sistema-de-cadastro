9d8d7657867d6869d2d1498a9f8d01945b61ec3b
Leonardo-Rendano <62267208+Leonardo-Rendano@users.noreply.github.com>
2020-07-13 18:53:31 -0300

    Altteração na função de salvar produtos

--- sistema-de-cadastro/src/app/produtoService.js
+++ sistema-de-cadastro/src/app/produtoService.js
@@ -42,6 +42,17 @@ export default class ProdutoService {
     return JSON.parse(produtos)
   }
 
+  obterIndex = (sku) => {
+    let index = null;
+    this.obterProdutos().forEach((produto, i) => {
+      if(produto.sku === sku) {
+        index = i;
+      }
+    })
+  }
+  return(index)
+
+
   salvar = (produto) => {
     this.validar(produto)
 
@@ -53,7 +64,13 @@ export default class ProdutoService {
       produtos = JSON.parse(produtos) //JSON.parse para converter a STRING em ARRAY!
     }
 
-    produtos.push(produto);
+    const index = this.obterIndex(produto.sku)
+      if(index === null) {
+        produtos.push(produto);
+      } else {
+        produtos[index] = produto
+      }
+    
 
     localStorage.setItem(PRODUTOS, JSON.stringify(produtos)) //JSON.stringify para converter o ARRAY em STRING!
   }
